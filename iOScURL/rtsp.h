4c1091a1db1af195aa9c7cf61bdfa4753e344280
Paul Uhn <paul.uhn@gmail.com>
2015-01-20 11:47:48 -0600

    Add rtsp.h and rtsp.m

---
+++ iOScURL/rtsp.h
@@ -0,0 +1,13 @@
+//
+//  rtsp.h based on rtsp.c
+//  iOScURL
+//
+//  Created by Paul Uhn on 1/20/15.
+//  Copyright (c) 2015 Paul Uhn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface rtsp : NSObject
+
+@end
---
+++ iOScURL/rtsp.m
@@ -0,0 +1,13 @@
+//
+//  rtsp.m based on rtsp.c
+//  iOScURL
+//
+//  Created by Paul Uhn on 1/20/15.
+//  Copyright (c) 2015 Paul Uhn. All rights reserved.
+//
+
+#import "rtsp.h"
+
+@implementation rtsp
+
+@end
